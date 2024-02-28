7ede38a567cfb63a05f26ac48eac438eebbdb758
liaozetao <1107136310@qq.com>
2024-02-28 18:07:01 +0800

    修改定时拉取逻辑

--- task/hentai321_task.py
+++ task/hentai321_task.py
@@ -4,7 +4,7 @@ from spiders.hentai321_spider import HenTai321Spider
 def execute():
     print('==== hentai321 comic task start ====')
     try:
-        HenTai321Spider().parse()
+        HenTai321Spider().job()
     except Exception as e:
         print(e)
     print('==== fhentai321 comic task end ====')
