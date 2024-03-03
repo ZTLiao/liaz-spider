84149005930881846402f6893f3a61e7076c7eae
liaozetao <1107136310@qq.com>
2024-03-03 12:09:54 +0800

    修复哔哩轻小说拉取问题

--- spiders/bilinovel_spider.py
+++ spiders/bilinovel_spider.py
@@ -388,6 +388,7 @@ class BiliNovelSpider:
             options.add_argument('--headless')
             options.add_argument('--remote-debugging-port=9222')
             browser = uc.Chrome(options=options)
+            time.sleep(1)
             browser.get(xiao_shuo_url)
             time.sleep(5)
             element = browser.find_element(value='searchkey')
@@ -396,18 +397,10 @@ class BiliNovelSpider:
             time.sleep(2)
             xiao_shuo_response_text = browser.page_source
             xiao_shuo_soup = bs4.BeautifulSoup(xiao_shuo_response_text, 'html.parser')
-            xiao_shuo_items = xiao_shuo_soup.select('li.book-li a')
-            if len(xiao_shuo_items) == 0:
-                return
-            for xiao_shuo_item in xiao_shuo_items:
-                detail_uri: str = xiao_shuo_item.get('href')
-                if not detail_uri.startswith('/novel'):
-                    print(detail_uri)
-                    continue
-                detail_url = self.domain + detail_uri
-                xiao_shuo_detail_response = requests.get(detail_url)
-                xiao_shuo_detail_response_text = xiao_shuo_detail_response.text
-                xiao_shuo_detail_soup = bs4.BeautifulSoup(xiao_shuo_detail_response_text, 'html.parser')
+            book_title_items = xiao_shuo_soup.select('h1.book-title')
+            if len(book_title_items) == 1 and len(book_title_items[0].text) != 0:
+                print('title : ', book_title_items[0].text)
+                xiao_shuo_detail_soup = xiao_shuo_soup
                 xiao_shuo_detail_item = xiao_shuo_detail_soup.select('div.book-detail-info .book-layout')[0]
                 title = xiao_shuo_detail_item.find('h1').text.replace('\'', '\\\'')
                 cover = xiao_shuo_detail_item.find('img').get('src')
@@ -549,6 +542,160 @@ class BiliNovelSpider:
                                 self.novel_chapter_item_db.save(novel_chapter_id, novel_id, path, page_index)
                                 self.novel_subscribe_db.upgrade(novel_id)
                 self.redis_util.delete(NOVEL_DETAIL + str(novel_id))
+            else:
+                xiao_shuo_items = xiao_shuo_soup.select('li.book-li a')
+                if len(xiao_shuo_items) == 0:
+                    return
+                for xiao_shuo_item in xiao_shuo_items:
+                    detail_uri: str = xiao_shuo_item.get('href')
+                    if not detail_uri.startswith('/novel'):
+                        print(detail_uri)
+                        continue
+                    detail_url = self.domain + detail_uri
+                    xiao_shuo_detail_response = requests.get(detail_url)
+                    xiao_shuo_detail_response_text = xiao_shuo_detail_response.text
+                    xiao_shuo_detail_soup = bs4.BeautifulSoup(xiao_shuo_detail_response_text, 'html.parser')
+                    xiao_shuo_detail_item = xiao_shuo_detail_soup.select('div.book-detail-info .book-layout')[0]
+                    title = xiao_shuo_detail_item.find('h1').text.replace('\'', '\\\'')
+                    cover = xiao_shuo_detail_item.find('img').get('src')
+                    print(cover)
+                    novel_id = self.novel_db.get_novel_id(title)
+                    if novel_id is None or novel_id == 0:
+                        file_name = self.file_item_handler.download(cover)
+                        if file_name is not None:
+                            cover = self.file_item_handler.upload(bucket.COVER, file_name, file_type.IMAGE_JPEG)
+                        print(cover)
+                        if cover is None:
+                            cover = ''
+                    author = xiao_shuo_detail_item.select('span.authorname a')[0].text
+                    self.author_db.save(author)
+                    author_id = self.author_db.get_author_id(author)
+                    category_items = xiao_shuo_detail_item.select('span.tag-small-group em.tag-small a')
+                    category_ids = []
+                    category_str = ''
+                    category_index = 0
+                    for category_item in category_items:
+                        category = category_item.text.strip()
+                        category = category.replace('\'', '\\\'').replace(',', '').replace('#', '').replace('\n',
+                                                                                                            '').strip()
+                        self.category_db.save(category)
+                        category_id = self.category_db.get_category_id(category)
+                        category_ids.append(str(category_id))
+                        category_str += category
+                        if category_index != len(category_items) - 1:
+                            category_str += ','
+                        category_index += 1
+                    category_id_str = ','.join(category_ids)
+                    description = xiao_shuo_detail_soup.select('section.book-summary content')[0].text
+                    print('title : ', title, ', description : ', description)
+                    self.novel_db.save(title, cover, description, 1, category_id_str, category_str, str(author_id),
+                                       author, 0, '')
+                    novel_id = self.novel_db.get_novel_id(title)
+                    asset_key = title + '|' + author
+                    self.asset_db.save(asset_key, 2, title, cover, novel_id, category_id_str, str(author_id))
+                    volume_uri = xiao_shuo_detail_soup.select('div.module-merge .book-status')[0].get('href')
+                    volume_url = self.domain + volume_uri
+                    print(volume_url)
+                    xiao_shuo_volume_response = requests.get(volume_url)
+                    xiao_shuo_volume_response_text = xiao_shuo_volume_response.text
+                    xiao_shuo_volume_soup = bs4.BeautifulSoup(xiao_shuo_volume_response_text, 'html.parser')
+                    volume_items = xiao_shuo_volume_soup.select('div.catalog-volume')
+                    volume_index = 0
+                    for volume_item in volume_items:
+                        volume_index += 1
+                        volume_name = volume_item.find('h3').text.replace('\'', '\\\'')
+                        self.novel_volume_db.save(novel_id, volume_name, volume_index)
+                        novel_volume_id = self.novel_volume_db.get_novel_volume_id(novel_id, volume_name)
+                        chapter_items = volume_item.select('.chapter-li-a')
+                        chapter_index = self.novel_chapter_db.get_seq_no_by_novel_volume_id(novel_id, novel_volume_id)
+                        for chapter_item in chapter_items:
+                            if system.global_vars.application.get_close_status() == status.YES:
+                                print('bili novel is close.')
+                                return
+                            chapter_index += 1
+                            chapter_name = chapter_item.text.replace('\'', '\\\'')
+                            count = self.novel_chapter_db.count_by_novel_volume_id(novel_id, novel_volume_id,
+                                                                                   chapter_name)
+                            self.novel_chapter_db.save_by_novel_volume_id(novel_id, novel_volume_id, chapter_name,
+                                                                          chapter_index)
+
+                            novel_chapter_id = self.novel_chapter_db.get_novel_chapter_id_by_novel_volume_id(
+                                novel_id,
+                                novel_volume_id,
+                                chapter_name)
+                            if count == 0 and novel_chapter_id is not None:
+                                self.asset_db.update(novel_id, 2, chapter_name, novel_chapter_id)
+                                self.novel_db.upgrade(novel_id)
+                            novel_chapter_id = self.novel_chapter_db.get_novel_chapter_id_by_novel_volume_id(
+                                novel_id,
+                                novel_volume_id,
+                                chapter_name)
+                            chapter_uri: str = chapter_item.get('href')
+                            if not chapter_uri.endswith('.html'):
+                                print('volume name : ', volume_name, ', chapter uri : ', chapter_uri)
+                                continue
+                            chapter_url = self.domain + chapter_uri
+                            print(chapter_url)
+                            xiao_shuo_chapter_response = requests.get(chapter_url, headers={
+                                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
+                                'Accept-Language': 'zh-CN,zh;q=0.9',
+                            })
+                            xiao_shuo_chapter_response_text = xiao_shuo_chapter_response.text
+                            time.sleep(2)
+                            xiao_shuo_chapter_soup = bs4.BeautifulSoup(xiao_shuo_chapter_response_text, 'html.parser')
+                            img_tags = xiao_shuo_chapter_soup.find_all('img')
+                            page_index = 0
+                            if len(img_tags) != 0:
+                                for img in img_tags:
+                                    path = img.get('data-src')
+                                    print(path)
+                                    page_index += 1
+                                    novel_chapter_id = self.novel_chapter_db.get_novel_chapter_id_by_novel_volume_id(
+                                        novel_id, novel_volume_id, chapter_name)
+                                    page_count = self.novel_chapter_item_db.count(novel_chapter_id, novel_id,
+                                                                                  page_index)
+                                    if page_count == 0:
+                                        file_name = self.file_item_handler.download(path, headers={
+                                            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
+                                            'Referer': self.domain,
+                                        })
+                                        if file_name is not None:
+                                            path = self.file_item_handler.upload(bucket.INSET, file_name,
+                                                                                 file_type.IMAGE_JPEG)
+                                        print(path)
+                                        self.novel_chapter_item_db.save(novel_chapter_id, novel_id, path,
+                                                                        page_index)
+                                        self.novel_subscribe_db.upgrade(novel_id)
+                            content_items = xiao_shuo_chapter_soup.select('div#acontentz')
+                            if len(content_items) > 0:
+                                content_items = content_items[0]
+                            print(content_items)
+                            content = ''
+                            for content_item in content_items.select('p, br'):
+                                tag: str = content_item.prettify()
+                                text = content_item.text
+                                if len(text) != 0:
+                                    if 'br' in tag:
+                                        content += '\n'
+                                    else:
+                                        content += content_item.text + '\n'
+                            content = replace_secret_text(content)
+                            if len(content) == 0:
+                                continue
+                            page_index += 1
+                            page_count = self.novel_chapter_item_db.count(novel_chapter_id, novel_id,
+                                                                          page_index)
+                            if page_count == 0:
+                                file_name = self.file_item_handler.write_content(content)
+                                path = None
+                                if file_name is not None:
+                                    path = self.file_item_handler.upload(bucket.NOVEL, file_name,
+                                                                         file_type.TEXT_PLAIN)
+                                print(path)
+                                if path is not None:
+                                    self.novel_chapter_item_db.save(novel_chapter_id, novel_id, path, page_index)
+                                    self.novel_subscribe_db.upgrade(novel_id)
+                    self.redis_util.delete(NOVEL_DETAIL + str(novel_id))
         except Exception as e:
             print(e)
 
